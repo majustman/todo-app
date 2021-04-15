66a3bfbf8927f5646c9e941e3f53132902fd0678
Samatt90 <samatt90@gmail.com>
2021-04-15 12:12:29 +0600

    feat: handlers

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"log"
 
 	"github.com/majustman/todo-app"
+	"github.com/majustman/todo-app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
