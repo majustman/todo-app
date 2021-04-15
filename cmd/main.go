d39f72305ef409b1d60c0df2e2b25fdcb9a1d407
Samatt90 <samatt90@gmail.com>
2021-04-15 12:34:54 +0600

    feat: repository and services

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/majustman/todo-app/pkg/repository"
+	"github.com/majustman/todo-app/pkg/service"
+
 	"github.com/majustman/todo-app"
 	"github.com/majustman/todo-app/pkg/handler"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
