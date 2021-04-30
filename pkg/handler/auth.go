fadd9016a0ad8d785461ca44c807b3eed6efdd94
samatt90 <samatt90@gmail.com>
2021-04-30 18:56:37 +0600

    feat: authorization

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,11 +1,18 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/majustman/todo-app"
 )
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
 
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+	}
 }
 
 func (h *Handler) signIn(c *gin.Context) {
---
+++ pkg/service/auth.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"github.com/majustman/todo-app"
+	"github.com/majustman/todo-app/pkg/repository"
+)
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	return s.repo.CreateUser(user)
+}
