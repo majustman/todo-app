7a94d5c9dc8a12dbf7f4ffecfd00e566b15659b1
samatt90 <samatt90@gmail.com>
2021-04-30 19:39:24 +0600

    feat: password crypt

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -1,9 +1,14 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
+
 	"github.com/majustman/todo-app"
 	"github.com/majustman/todo-app/pkg/repository"
 )
+
+const salt = "lklksdjflkds5+65+66dsfsdf"
 
 type AuthService struct {
 	repo repository.Authorization
@@ -14,5 +19,13 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 }
 
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = s.generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
